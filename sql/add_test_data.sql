1b5672149abe6dd41f2a3e03577f24fe524b447b
hyytiala <otto.hyytiala@gmail.com>
2016-04-04 13:44:53 +0300

    Lisätty taulujen luontilauseet ja testidatan lisäyslauseet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,4 @@
--- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Asiakas (nimi, osoite, postinumero, postipaikka, puhelin, email) VALUES ('Esa Korhonen', 'Pihlajatie 26', '00250', 'Helsinki', '040586412', 'esa@esa.fi');
+INSERT INTO Asiakas (nimi, osoite, postinumero, postipaikka, puhelin, email) VALUES ('Pertti Jokunen', 'Töölönkatu 20', '00100', 'Helsinki', '040468215', 'jokunen@mail.fi');
+INSERT INTO Tyomies (nimi, salasana, puhelin) VALUES ('Antti', 'antti123', '045864762');
+INSERT INTO Kohde (osoite, aloitettu, tila, kuvaus) VALUES ('Mariankatu 15', '14.2.2014', 'kesken', 'Pientä pintaremonttia')
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,35 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Asiakas(
+	id SERIAL PRIMARY KEY,
+	nimi varchar(50) NOT NULL,
+	osoite varchar(100) NOT NULL,
+	postinumero varchar(10) NOT NULL,
+	postipaikka varchar(50) NOT NULL,
+	puhelin varchar(50),
+	email varchar(50)
+);
+
+CREATE TABLE Tyomies(
+	id SERIAL PRIMARY KEY,
+	nimi varchar(50) NOT NULL,
+	salasana varchar(50) NOT NULL,
+	puhelin varchar(50) NOT NULL,
+	tunnit DECIMAL DEFAULT 0
+);
+
+CREATE TABLE Kohde(
+	id SERIAL PRIMARY KEY,
+	osoite varchar(100) NOT NULL,
+	aloitettu DATE NOT NULL,
+	tila varchar(10) NOT NULL,
+	kuvaus varchar(200),
+	katselu boolean DEFAULT FALSE,
+	asiakas_id INTEGER REFERENCES Asiakas(id)
+);
+
+CREATE TABLE Merkinta(
+	paivays DATE NOT NULL,
+	tunnit DECIMAL NOT NULL,
+	kuvaus varchar(100),
+	Kohde INTEGER REFERENCES Kohde(id),
+	tyomies INTEGER REFERENCES Tyomies(id)
+);
